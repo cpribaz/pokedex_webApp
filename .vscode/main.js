356ce5b46013c3c73b69deec46fcda7bce145883
cpribaz <cpribaz@uwo.ca>
2022-09-20 13:01:38 -0400

    [Lab 1] - search by number functionality added

--- .vscode/main.js
+++ .vscode/main.js
@@ -1,5 +1,5 @@
 function searchName(){
-    var input, filter, ul, li, a, i, txtValue;
+    var input, filter, ul, li, i, txtValue;
     input = document.getElementById('searchName');
     filter = input.value.toUpperCase();
     ul = document.getElementById("ul");
@@ -9,6 +9,25 @@ function searchName(){
     for (i = 0; i < li.length; i++) {
         h4 = li[i].getElementsByTagName("h4")[0];
         txtValue = h4.textContent || h4.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function searchNumber(){
+    var input, filter, ul, li, i, txtValue;
+    input = document.getElementById('searchNumber');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("ul");
+    li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        num = li[i].getElementsByClassName("number")[0];
+        txtValue = num.textContent || num.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
