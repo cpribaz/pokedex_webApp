551c3adebe6eb6634a6c932b96d20b83642f5d43
cpribaz <cpribaz@uwo.ca>
2022-09-20 12:40:09 -0400

    [Lab 1] - search by name functionality added

---
+++ .vscode/main.js
@@ -0,0 +1,18 @@
+function searchName(){
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchName');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("ul");
+    li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        h4 = li[i].getElementsByTagName("h4")[0];
+        txtValue = h4.textContent || h4.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
