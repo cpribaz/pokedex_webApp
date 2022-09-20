ec44a0da184e905a0063142633168c7210638109
cpribaz <cpribaz@uwo.ca>
2022-09-20 18:20:40 -0400

    [Lab 1] - pokemon stat array and search function added

--- .vscode/main.js
+++ .vscode/main.js
@@ -1,3 +1,35 @@
+let pokemon = [
+    {"Type 1":"grass", "Type 2":"poison", "Base ATK":118, "Base DEF":118, "Base STA":90},
+    {"Type 1":"grass", "Type 2":"poison", "Base ATK":151, "Base DEF":151, "Base STA":120},
+    {"Type 1":"grass", "Type 2":"poison", "Base ATK":198, "Base DEF":198, "Base STA":160},
+    {"Type 1":"fire", "Type 2":"N/A", "Base ATK":116, "Base DEF":96, "Base STA":78},
+    {"Type 1":"fire", "Type 2":"N/A", "Base ATK":158, "Base DEF":129, "Base STA":116},
+    {"Type 1":"fire", "Type 2":"flying", "Base ATK":223, "Base DEF":176, "Base STA":156},
+    {"Type 1":"water", "Type 2":"N/A", "Base ATK":94, "Base DEF":122, "Base STA":88},
+    {"Type 1":"water", "Type 2":"N/A", "Base ATK":126, "Base DEF":155, "Base STA":118},
+    {"Type 1":"water", "Type 2":"N/A", "Base ATK":171, "Base DEF":210, "Base STA":158},
+    {"Type 1":"bug", "Type 2":"N/A", "Base ATK":55, "Base DEF":62, "Base STA":90},
+    {"Type 1":"bug", "Type 2":"N/A", "Base ATK":45, "Base DEF":94, "Base STA":100},
+    {"Type 1":"bug", "Type 2":"flying", "Base ATK":167, "Base DEF":151, "Base STA":120},
+    {"Type 1":"bug", "Type 2":"poison", "Base ATK":63, "Base DEF":55, "Base STA":80},
+    {"Type 1":"bug", "Type 2":"poison", "Base ATK":46, "Base DEF":86, "Base STA":90},
+    {"Type 1":"bug", "Type 2":"poison", "Base ATK":169, "Base DEF":150, "Base STA":130},
+    {"Type 1":"normal", "Type 2":"flying", "Base ATK":85, "Base DEF":76, "Base STA":80},
+    {"Type 1":"normal", "Type 2":"flying", "Base ATK":117, "Base DEF":108, "Base STA":126},
+    {"Type 1":"normal", "Type 2":"flying", "Base ATK":166, "Base DEF":157, "Base STA":166},
+    {"Type 1":"normal", "Type 2":"N/A", "Base ATK":103, "Base DEF":70, "Base STA":60},
+    {"Type 1":"normal", "Type 2":"N/A", "Base ATK":161, "Base DEF":144, "Base STA":110},
+];
+
+function displayStats(clickedID){
+    var type1 = "Type 1: " + pokemon[clickedID-1]["Type 1"];
+    var type2 = "Type 2: " + pokemon[clickedID-1]["Type 2"];
+    var baseATK = "Base ATK: " + pokemon[clickedID-1]["Base ATK"];
+    var baseDEF = "Base DEF: " + pokemon[clickedID-1]["Base DEF"];
+    var baseSTA = "Base STA: " + pokemon[clickedID-1]["Base STA"];
+    alert(type1 + "\r\n" + type2 + "\r\n" + baseATK + "\r\n" + baseDEF + "\r\n" + baseSTA);
+}
+
 function searchName(){
     var input, filter, ul, li, i, txtValue;
     input = document.getElementById('searchName');
